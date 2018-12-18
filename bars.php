b74a8ad4345ab57c59f9f918fcabbbbce5377391
anuroop shrestha <anuroopshr@gmail.com>
2018-12-18 14:47:26 +0545

    sync

---
+++ bars.php
@@ -0,0 +1,26 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+          <div class="sidebar">
+              <h6>Filters</h6>
+              <div class="filter-options">
+                <input type="radio">Cp Pick
+                <input type="radio">Open Now
+              </div>
+              <div class="filter-group">
+                <ul>
+                  <li>Area</li>
+                  <li>Styles</li>
+                  <li>Features</li>
+                  <li>Price</li>
+                  <li>Rating</li>
+                </ul>
+              </div>
+          </div>
+        </div>
+      </div>
+</div>
+
+<?php include 'footer.php'; ?>
