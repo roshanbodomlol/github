7ac74d5b156d6d3c7d839973fde2d910c6f8291b
Roshan Maharjan <roshan.bodom@gmail.com>
2018-12-19 14:19:54 +0545

    review tab

---
+++ templates/list/1.php
@@ -0,0 +1,15 @@
+<div class="col-sm-3">
+  <div class="k-list _basic">
+    <div class="_img">
+      <img src="img/list/1b.jpg" alt="">
+    </div>
+    <div class="_dets">
+      <div class="_title">
+          ALBERTO LOUNGE IS THE BUZZING NEW ITALIAN TRATTORIA FROM THE RESTAURANT HUBERT CREW
+      </div>
+      <div class="_icon">
+        <i class="fas fa-glass-martini-alt"></i>
+      </div>
+    </div>
+  </div>
+</div>
