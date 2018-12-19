6c334c87dd9b54f506a9e4a651c241c71b76aa54
anuroop shrestha <anuroopshr@gmail.com>
2018-12-19 17:13:07 +0545

    bar

---
+++ bar.php
@@ -0,0 +1,47 @@
+<?php include 'header.php'; ?>
+
+
+    <!--Listing-->
+      <div class="full">
+          <div class="container">
+          <div class="listing-wrapper">
+              <div class="listing-menu"> <!--flex-->
+                <div class="list">
+                    <i class="fas fa-bars"></i>
+                  <span>List</span>
+                </div>
+                <div class="filer-list">
+                  <ul class="list-inline">
+                    <li class="list-inline-item">Sort by:</li>
+                    <li class="list-inline-item">All</li>
+                    <li class="list-inline-item">Newest</li>
+                    <li class="list-inline-item">Nearest</li>
+                    <li class="list-inline-item">Most Viewed</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          <!--Sidebar Filter-->
+            <div class="sidebar">
+              <div class="dark-sidebar">
+                <h6>Filters</h6>
+                <div class="k-input">
+                  <div class="inner-k-input">
+                    <label>
+                      <input type="checkbox">
+                      <span>Cp Pick (11)</span>
+                    </label>
+                  </div>
+                  <div class="inner-k-input">
+                    <label>
+                      <input type="checkbox">
+                      <span>Open Now (11)</span>
+                      </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+      </div>
+</div>
+
+<?php include 'footer.php'; ?>
--- bars.php
+++ bars.php
@@ -1,33 +1,182 @@
 <?php include 'header.php'; ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-md-2">
-      <div class="sidebar">
-        <div class="dark-sidebar">
-          <h6>Filters</h6>
-          <div class="sub_option">
-            <label>
-              <input type="checkbox">
-              <span>Cp Pick (11)</span>
-            </label>
+<div class="listing-page">
+    <div class="container">
+        <div class="row">
+          <!--Listing-->
+          <div class="col-md-9">
+            <div class="listing-wrapper">
+              <!--listing menu-->
+              <div class="listing-menu">
+                <div class="list">
+                    <i class="fas fa-bars"></i>
+                  <span>List</span>
+                </div>
+                <div class="filter-list">
+                  <ul class="list-inline bordered">
+                    <li class="list-inline-item">Sort by:</li>
+                    <li class="list-inline-item"><a href="#">All</a></li>
+                    <li class="list-inline-item"><a href="#">Newest</a></li>
+                    <li class="list-inline-item"><a href="#">Nearest</a></li>
+                    <li class="list-inline-item"><a href="#">Most Viewed</a></li>
+                  </ul>
+                </div>
+              </div>
+              <!--listing content-->
+              <div class="listings">
+                  <div class="listing-item"> <!--item 1-->
+                      <div class="row">
+                          <div class="col-md-5">
+                            <div class="listing-item-image">
+                                <img src="img/restaurant.jpg" alt="">
+                            </div>
+                          </div>
+                          <!-- <div class="col-md-1">
+                              <i class="fas fa-wine-glass-alt"></i>
+                          </div> -->
+                          <div class="col-md-7">
+                            <div class="listing-item-content">
+                                <h4>Busy Bee</h4>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <p class="address">Thamel, Kathmandu</p>
+                                <p>
+                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                  labore et dolore magna aliqua. Nisl pretium fusce id velit. Arcu odio ut sem nulla pharetra 
+                                  diam sit 
+                                </p>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                  <div class="listing-item"> <!--item 2-->
+                      <div class="row">
+                          <div class="col-md-5">
+                            <div class="listing-item-image">
+                                <img src="img/casino.jpg" alt="">
+                            </div>
+                          </div>
+                          <div class="col-md-7">
+                            <div class="listing-item-content">
+                                <h4>Purple Haze</h4>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <p class="address">Thamel, Kathmandu</p>
+                                <p>
+                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                  labore et dolore magna aliqua. Nisl pretium fusce id velit. Arcu odio ut sem nulla pharetra 
+                                  diam sit 
+                                </p>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                  <div class="listing-item"><!--item 3-->
+                      <div class="row">
+                          <div class="col-md-5">
+                            <div class="listing-item-image">
+                                <img src="img/restaurant.jpg" alt="">
+                            </div>
+                          </div>
+                          <div class="col-md-7">
+                            <div class="listing-item-content">
+                                <h4>Club 25 Hours</h4>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <p class="address">Thamel, Kathmandu</p>
+                                <p>
+                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                  labore et dolore magna aliqua. Nisl pretium fusce id velit. Arcu odio ut sem nulla pharetra 
+                                  diam sit 
+                                </p>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                  <div class="listing-item"> <!--item 4-->
+                      <div class="row">
+                          <div class="col-md-5">
+                            <div class="listing-item-image">
+                                <img src="img/casino.jpg" alt="">
+                            </div>
+                          </div>
+                          <div class="col-md-7">
+                            <div class="listing-item-content">
+                                <h4>Reggae Bar</h4>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <p class="address">Thamel, Kathmandu</p>
+                                <p>
+                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                  labore et dolore magna aliqua. Nisl pretium fusce id velit. Arcu odio ut sem nulla pharetra 
+                                  diam sit 
+                                </p>
+                            </div>
+                          </div>
+                        </div>
+                  </div>
+                </div>
+            </div>
           </div>
-          <div class="sub_option">
-            <label>
-              <input type="checkbox">
-              <span>Open Now (11)</span>
-              </label>
-          </div>
-          <div class="sub_option">
-            <label>
-              <input type="checkbox">
-              <span>Open Now (11)</span>
-              </label>
+          <!--Sidebar Top Filter-->
+          <div class="col-md-3">
+            <div class="sidebar">
+              <div class="dark-sidebar">
+                <h6>Filters</h6>
+                <div class="k-input">
+                  <div class="inner-k-input">
+                    <label>
+                      <input type="checkbox">
+                      <span>Cp Pick (11)</span>
+                    </label>
+                  </div>
+                  <div class="inner-k-input">
+                    <label>
+                      <input type="checkbox">
+                      <span>Open Now (11)</span>
+                      </label>
+                  </div>
+                </div>
+              </div>
+              <!--Mini Extra Filter-->
+              <div class="extra-filters"> 
+                <div class="single-filter">
+                  <span>Area</span>
+                  <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class="single-filter">
+                  <span>Styles</span>
+                  <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class="single-filter">
+                  <span>Features</span>
+                  <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class="single-filter">
+                  <span>Price</span>
+                  <i class="fas fa-chevron-down"></i>
+                </div>
+                <div class="single-filter">
+                  <span>Rating</span>
+                  <i class="fas fa-chevron-down"></i>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
 </div>
 
 <?php include 'footer.php'; ?>
